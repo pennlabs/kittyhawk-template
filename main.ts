092f643bf0503b0183e01ef713cfc8eddc542e01
Brandon Wang <brandonbigbrother@gmail.com>
2020-11-08 12:07:31 -0600

    bumping kittyhawk version

--- main.ts
+++ main.ts
@@ -4,17 +4,20 @@ import { Construct } from 'constructs';
 export function buildChart(scope: Construct) {
 
   const release_name = "project-name";
+  const tag = process.env.IMAGE_TAG;
 
   // Sample Application (Replace this with your actual configuration)
   new Application(scope, `${release_name}-webapp`, {
-      image: 'pennlabs/project-image:latest',
+      image: 'pennlabs/project-image',
+      tag: tag,
       port: 80
   });
 
   // Sample Cronjob
   new CronJob(scope, `${release_name}-cronjob`, {
         schedule: "0 8 * * *",
-        image: "pennlabs/cronjob:latest",
+        image: "pennlabs/cronjob",
+        tag: tag,
         secret: "my-secret",
         cmd: ["python", "manage.py", "launch"],
   });
