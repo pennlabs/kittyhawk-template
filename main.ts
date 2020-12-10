48fd911d2114be307c5aed98030a625b0303e5ce
Brandon Wang <brandonbigbrother@gmail.com>
2020-12-09 19:56:14 -0600

    version bump

--- main.ts
+++ main.ts
@@ -3,21 +3,17 @@ import { Construct } from 'constructs';
 
 export function buildChart(scope: Construct) {
 
-  const release_name = "project-name";
-  const tag = process.env.IMAGE_TAG;
 
   // Sample Application (Replace this with your actual configuration)
-  new Application(scope, `${release_name}-webapp`, {
+  new Application(scope, 'webapp', {
       image: 'pennlabs/project-image',
-      tag: tag,
       port: 80
   });
 
   // Sample Cronjob
-  new CronJob(scope, `${release_name}-cronjob`, {
+  new CronJob(scope, 'cronjob', {
         schedule: "0 8 * * *",
-        image: "pennlabs/cronjob",
-        tag: tag,
+        image: "pennlabs/cronjob-image",
         secret: "my-secret",
         cmd: ["python", "manage.py", "launch"],
   });
